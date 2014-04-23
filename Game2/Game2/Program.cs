3adcd56b1469efbaf660e18e7b3423c37385c38d
Chris3037 <Chris3037@gmail.com>
2014-04-22 23:14:09 -0600

    Initial Upload

---
+++ Game2/Game2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Game2
+{
+    class Program
+    {
+        //Public variables
+        static string userName = "";
+        static string input = "";
+
+        static void Main(string[] args)
+        {
+            Welcome();
+        }
+
+        //Welcome Function
+        static void Welcome()
+        {
+            Console.WriteLine("Please enter your name");
+            input = Console.ReadLine();
+            userName = input;
+            Console.Clear();
+            Console.WriteLine("Welcome to the game " + userName);
+
+            End();
+        }
+
+        //End Function
+        static void End()
+        {
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+    }
+}
