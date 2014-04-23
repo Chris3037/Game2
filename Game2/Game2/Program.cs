ea91c28debc131a919e5499d545cc4a92e9f7823
Chris3037 <Chris3037@gmail.com>
2014-04-22 23:54:20 -0600

    V 1.0

--- Game2/Game2/Program.cs
+++ Game2/Game2/Program.cs
@@ -15,6 +15,11 @@ namespace Game2
 
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            TextPrint("White on blue.");
+            Console.WriteLine("Another Line");
+            Console.ResetColor();
             Welcome();
         }
 
@@ -28,6 +33,61 @@ namespace Game2
             Console.WriteLine("Welcome to the game " + userName);
 
             End();
+        }
+
+        //Song Function
+        static void Song()
+        {
+            //E6
+            Console.Beep(1318, 400);
+            //B5
+            Console.Beep(988, 200);
+            //C6
+            Console.Beep(1046, 200);
+            //D6
+            Console.Beep(1175, 400);
+            //C6
+            Console.Beep(1046, 200);
+            //B5
+            Console.Beep(988, 200);
+            //A5
+            Console.Beep(880, 400);
+            Console.Beep(880, 200);
+            Console.Beep(1046, 200);
+            Console.Beep(1318, 400);
+            Console.Beep(1175, 200);
+            Console.Beep(1046, 200);
+            Console.Beep(988, 600);
+            Console.Beep(988, 200);
+            Console.Beep(1175, 400);
+            Console.Beep(1318, 400);
+            Console.Beep(1046, 400);
+            Console.Beep(880, 400);
+            Console.Beep(880, 400);
+            Thread.Sleep(600);
+            Console.Beep(1175, 600);
+            //F6
+            Console.Beep(1397, 200);
+            //A6
+            Console.Beep(1760, 400);
+            //G6
+            Console.Beep(1568, 200);
+            //F6
+            Console.Beep(1397, 200);
+            Console.Beep(1318, 200);
+            Console.Beep(1318, 400);
+            Console.Beep(1046, 200);
+            Console.Beep(1318, 400);
+            Console.Beep(1175, 200);
+            Console.Beep(1046, 200);
+            Console.Beep(988, 400);
+            Console.Beep(988, 200);
+            Console.Beep(1046, 200);
+            Console.Beep(1175, 400);
+            Console.Beep(1318, 400);
+            Console.Beep(1046, 400);
+            Console.Beep(880, 400);
+            Console.Beep(880, 400);
         }
 
         //End Function
